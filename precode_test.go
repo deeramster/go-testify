df8ae8232a0edf5ba2a83086481d881dff8654d0
Iskander Alimov <rocketikman@yandex.ru>
2024-06-23 19:30:42 +0300

    test

---
+++ precode_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMainHandlerValidRequest(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/?city=moscow&count=2", nil)
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, http.StatusOK, responseRecorder.Code)
+	assert.NotEmpty(t, responseRecorder.Body.String())
+}
+
+func TestMainHandlerInvalidCity(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/?city=london&count=2", nil)
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+	assert.Equal(t, "wrong city value", responseRecorder.Body.String())
+}
+
+func TestMainHandlerCountMoreThanTotal(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/?city=moscow&count=10", nil)
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, http.StatusOK, responseRecorder.Code)
+	expectedAnswer := strings.Join(cafeList["moscow"], ",")
+	assert.Equal(t, expectedAnswer, responseRecorder.Body.String())
+}
